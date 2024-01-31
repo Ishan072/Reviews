6583a0e1dd34963a094717ecd043549bccb47671
Ishan <ishanbhardwaj1476@gmail.com>
2024-01-31 13:28:35 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
+import people from './data.js';
+import {FaChevronLeft,FaChevronRight,FaQuoteRight} from 'react-icons/fa';
 
 function App() {
+
+  const[index,setIndex] = useState(0);
+  const {name , job , image, text} = people[index];
+
+  const randomValue = () => {
+    setIndex(()=>{
+      let randomNumber = Math.floor(Math.random() * people.length);
+      if(randomNumber === index)
+      {
+        randomNumber = (randomNumber+2)%people.length;
+      }
+      return randomNumber;
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <article className='review'>
+        <div className='img-container'>
+          <img src={image} alt={name} className='person-img'/>
+          <span className='quote-icon'>
+            <FaQuoteRight/>
+          </span>
+        </div>
+        <h4 className='author'>{name}</h4>
+        <p className='job'>{job}</p>
+        <p className='info'>{text}</p>
+        <div className='btn-container'>
+          <button className='prev-btn' onClick={()=>{ setIndex((currentIndex)=>{
+              return (currentIndex!==0)?(currentIndex-=1)%(people.length):(currentIndex+=(people.length-1))%(people.length);
+          }) }}>
+            <FaChevronLeft/>
+          </button>
+          <button className='next-btn' onClick={()=>{ setIndex((currentIndex)=>{
+              return (currentIndex+=1)%(people.length);
+            }) }}>
+            <FaChevronRight/>
+          </button>
+        </div>
+        <button className='btn btn-hipster' onClick={randomValue}>Surprise Me</button>
+      </article>
+    </main>
   );
 }
 
